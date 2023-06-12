0fa7355377d246103decd3197e8a3e7b9eebec77
andreyurl54 <126441565+andreyurl54@users.noreply.github.com>
2023-06-12 16:54:56 +0300

    freetv.js

--- freetv.js
+++ freetv.js
@@ -1,6 +1,6 @@
 /*
 	Разобраться с иконками в ряд DONE
-	Вернуть ЮзерАгент - сломает выбор плеера на Android ? Lampa.Player.opened() && "namecomponent" == Lampa.Activity.active().component
+	Вернуть ЮзерАгент - сломает выбор плеера на Android ?
 */
 ;(function () {
 'use strict';
@@ -466,7 +466,7 @@ Lampa.SettingsApi.addParam({
 						type: 'select', 			//доступно select,input,trigger,title,static
 						values: {					//значения (слева) выставляемые в поле TVmenu через Storage, справа - их видимое название в меню
 							FREETV: 'FreeTV',
-							DIESEL: 'Дизель by YOSSO',
+							DIESEL: 'Дизель',
 							TVTEAM: 'Дизель Плюс'
 						},
 						default: 'FREETV'			//Здесь прописываем вариант по-умолчанию, а именно левую часть в VALUES (не значение, а имя параметра - слева!), иначе - undefined
