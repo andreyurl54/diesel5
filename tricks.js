23a7d2273e200648c8bc1d0a685fae6592f73841
andreyurl54 <126441565+andreyurl54@users.noreply.github.com>
2023-06-25 12:35:21 +0300

    tricks.js

--- tricks.js
+++ tricks.js
@@ -229,16 +229,16 @@ function add() {
 	    	Lampa.SettingsApi.addParam({
 	    		component: 'Multi_Menu_Component',
 	    		param: {
-	    			name: 'SpeedTest_alt_1',
+	    			name: 'OpenSpeedTestParam',
 	    			type: 'static', //доступно select,input,trigger,title,static
 	    		},
 	    		field: {
-	    			name: 'SpeedTest_1',
+	    			name: 'OpenSpeedTest',
 	    			description: 'Замер скорости интернет-соединения'
 	    		},
 	    		onRender: function (item) {
 	    			item.on('hover:enter', function(){
-					var modal = $('<div style="text-align:right;"><div style="min-height:360px;"><div style="width:100%;height:0;padding-bottom:50%;position:relative;"><iframe style="border:none;position:absolute;top:0;left:0;width:100%;height:100%;min-height:360px;border:none;overflow:hidden !important;" src="https://openspeedtest.com/speedtest?Run"></iframe></div></div></div>');
+					var modal = $('<div style="text-align:right;"><div style="min-height:360px;"><div style="width:100%;height:0;padding-bottom:50%;position:relative;"><iframe style="border:none;position:absolute;top:0;left:0;width:100%;height:100%;min-height:360px;border:none;overflow:hidden !important;" src="https://openspeedtest.com/?Run=5"></iframe></div></div></div>');
 		  			Lampa.Modal.open({
 		  				title: '',
 		  				html: modal,
