7ee85a4d9063e241adf10db02c19fcc074c72d6e
andreyurl54 <126441565+andreyurl54@users.noreply.github.com>
2023-06-28 22:11:05 +0300

    tricks.js

--- tricks.js
+++ tricks.js
@@ -727,11 +727,11 @@ if(Lampa.Storage.field('SISI_fix') == true) $("[data-action=sisi]").eq(0).show()
 				/*
 					var elementScreenSaver = $('.screensaver-chrome')
 					if (elementScreenSaver.length > 0){
-     						var inScript = '' +
+     						var inScript = '<script>' +
 						'let iframeHead = $("body > div.screensaver-layer > div > iframe").contents().find("head");' +
     						'let iframeCSS = '<style>[ng-if="isSimpleTopic"]{opacity: 0%!important;display: none;}</style>'';' +
     						'$(iframeHead).append(iframeCSS);' +
-						'frames[0].document.head.appendChild(iframeCSS);'
+						'frames[0].document.head.appendChild(iframeCSS);' + '</script>'
       					$('body').append(inScript);
 	   				clearInterval(notimedatescreenInterval);
 					}
