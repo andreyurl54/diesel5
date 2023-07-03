70fd27905d01f2f52f8146285d599aec3e9f8e8e
andreyurl54 <126441565+andreyurl54@users.noreply.github.com>
2023-07-03 14:33:19 +0300

    tricks.js

--- tricks.js
+++ tricks.js
@@ -467,7 +467,25 @@ if(Lampa.Storage.field('SISI_fix') == true) $("[data-action=sisi]").eq(0).show()
 		}
 		if(Lampa.Storage.field('Reloadbutton') == true) {
 				$('#CONSOLE').removeClass('hide');
-		}			
+		}		
+	/* Кнопка Выхода в верхнем баре */
+		var my_top_exit = '<svg version="1.1" id="exit" color="#fff" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"\n	 viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve">\n<g>\n	<path fill="currentColor" d="M256,5.1c138.6,0,250.9,112.3,250.9,250.9S394.6,506.9,256,506.9S5.1,394.6,5.1,256S117.4,5.1,256,5.1z\n		 M256,40.1C136.7,40.1,40.1,136.7,40.1,256S136.7,471.9,256,471.9S471.9,375.3,471.9,256S375.3,40.1,256,40.1z M311.4,176.6\n		c6.7-6.7,17.5-6.7,24.2,0c6.7,6.7,6.7,17.5,0,24.2l-55.1,55.1l55.1,55c6.7,6.7,6.7,17.5,0,24.2c-6.7,6.7-17.5,6.7-24.2,0L256.3,280\n		l-55.1,55.1c-6,6-15.4,6.6-22.1,1.8l-2.2-1.8c-6.7-6.7-6.7-17.5,0-24.2l55.1-55l-55.1-55c-6.7-6.7-6.7-17.5,0-24.2s17.5-6.7,24.2,0\n		l55.1,55.1L311.4,176.6z"/>\n</g>\n</svg>';
+		$('#app > div.head > div > div.head__actions').append(my_top_exit);
+
+		$('#my_top_exit').on('hover:enter hover:click hover:touch', function() {
+				if(Lampa.Platform.is('tizen')) tizen.application.getCurrentApplication().exit();
+				if(Lampa.Platform.is('webos')) window.close();
+				if(Lampa.Platform.is('android')) Lampa.Android.exit();
+				if(Lampa.Platform.is('orsay')) Lampa.Orsay.exit();
+				if(Lampa.Platform.is('browser')) window.close();
+		});
+		if(Lampa.Storage.field('Reloadbutton') == false) {
+				$('#my_top_exit').addClass('hide');
+		}
+		if(Lampa.Storage.field('Reloadbutton') == true) {
+				$('#my_top_exit').removeClass('hide');
+		}
+		
 /* End Кнопка Перезагрузки и Консоли*/
 
 
