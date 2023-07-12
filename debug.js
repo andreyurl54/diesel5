529d7a84ad568cb7f9b06e96eff7aa82059bb020
andreyurl54 <126441565+andreyurl54@users.noreply.github.com>
2023-07-12 23:00:01 +0300

    debug.js

--- debug.js
+++ debug.js
@@ -119,8 +119,8 @@ Lampa.SettingsApi.addComponent({
 						//checkPlugin('http://cub.watch/plugin/tmdb-proxy');
 						var myResult = checkPlugin('http://cub.watch/plugin/tmdb-proxy')
 						console.log('search', myResult);
-						//if (myResult) {$('div[data-name="TMDB"]').addClass('active')}
-						if (myResult) $('[data-name="TMDB"]').append('<div class="settings-param__status one"></div>').find('.settings-param__status').removeClass('active error wait').addClass('active')
+						if (myResult) {$('div[data-name="TMDB"]').append('<div class="settings-param__status one active"></div>')}
+						//if (myResult) $('[data-name="TMDB"]').append('<div class="settings-param__status one"></div>').find('.settings-param__status').removeClass('active error wait').addClass('active')
 						//if (myResult) $('[data-name="TMDB"]').find('.settings-param__status').removeClass('active error wait').addClass('active')
 						//if (checkPlugin) {console.log('search', 'Плагин найден!')} else {console.log('search', 'НЕ найден!')} 
 					}
