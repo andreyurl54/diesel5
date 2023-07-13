b986e5d12dd3785ca386f7d1f43cf1232cb2766d
andreyurl54 <126441565+andreyurl54@users.noreply.github.com>
2023-07-13 04:38:21 +0300

    debug.js

--- debug.js
+++ debug.js
@@ -18,7 +18,6 @@ function itemON(sourceURL, sourceName) {
                    script.src = sourceURL;
                    document.getElementsByTagName ('head')[0].appendChild (script);
 			setTimeout(function() {
-			   Lampa.Plugins.save()
 			   Lampa.Settings.update();
 			   Lampa.Noty.show("Плагин " + sourceName + " успешно установлен")
 			}, 300);
