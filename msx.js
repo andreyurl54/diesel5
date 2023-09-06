a85f1a77b4e5df530c4144c3b16180422252b373
andreyurl54 <126441565+andreyurl54@users.noreply.github.com>
2023-09-06 13:34:20 +0300

    msx.js

--- msx.js
+++ msx.js
@@ -1,6 +1,6 @@
 ;(function () {
     'use strict';
 
-If (Lampa.Platform.is('android')){Lampa.Noty.show('android!')} else {Lampa.Noty.show('Not Android')}
+If (Lampa.Platform.is('android')) Lampa.Noty.show('android!') else {Lampa.Noty.show('Not Android')}
 
 })();
