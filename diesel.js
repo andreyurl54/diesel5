cb736a8c8b8e01bae79d84801b191f84251b1920
andreyurl54 <126441565+andreyurl54@users.noreply.github.com>
2023-09-23 17:59:11 +0300

    diesel.js

--- diesel.js
+++ diesel.js
@@ -2714,6 +2714,30 @@ function configurePlaylist(i) {
 
 Lampa.Component.add(plugin.component, pluginPage);
 // Готовим настройки
+
+/* METRIKA */
+			(function(m, e, t, r, i, k, a) {
+				m[i] = m[i] || function() {
+					(m[i].a = m[i].a || []).push(arguments)
+				};
+				m[i].l = 1 * new Date();
+				for(var j = 0; j < document.scripts.length; j++) {
+					if(document.scripts[j].src === r) {
+						return;
+					}
+				}
+				k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
+			})
+			(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+			ym(93958290, "init", {
+				clickmap: true,
+				trackLinks: true,
+				accurateTrackBounce: true
+			});
+			var METRIKA = '<noscript><div><img src="https://mc.yandex.ru/watch/93958290" style="position:absolute; left:-9999px;" alt="" /></div></noscript>';
+			$('body').append(METRIKA);
+/* End METRIKA */	
+	
 Lampa.SettingsApi.addComponent(plugin);
 addSettings(
 	'trigger',
