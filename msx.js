b763eed70870e20fdbfa11490d06d645955165da
andreyurl54 <126441565+andreyurl54@users.noreply.github.com>
2023-10-20 06:19:15 +0300

    msx.js

--- msx.js
+++ msx.js
@@ -6,8 +6,10 @@ If (Lampa.Platform.is('noname')) Lampa.Noty.show('noname!');
 */
 var userAgent = navigator.userAgent;
 var agentFilter = userAgent.match(/Android/i);
-if (Lampa.Platform.is('noname')&&(agentFilter = 'Android')) {
-	Lampa.Noty.show('Установите АРК')
+if (Lampa.Platform.is('noname'){
+	if(agentFilter = 'Android')) {
+		Lampa.Noty.show('Установите АРК')
+	} 
 } else {
 	Lampa.Noty.show('notAndroid')
 }
