2491962e7886db667a09cc07d8ea5dbdb68e5264
andreyurl54 <126441565+andreyurl54@users.noreply.github.com>
2023-10-20 05:22:17 +0300

    msx.js

--- msx.js
+++ msx.js
@@ -1,7 +1,14 @@
 ;(function () {
     'use strict';
-
+/*
 If (Lampa.Platform.is('android')) Lampa.Noty.show('android!');
 If (Lampa.Platform.is('noname')) Lampa.Noty.show('noname!');
-
+*/
+var userAgent = navigator.userAgent;
+var agentFilter = userAgent.match(/Android/i);
+if (Lampa.Platform.is('noname')&&(agentFilter = 'Android')) {
+	Lampa.Noty.show('Установите АРК')
+} else {
+	Lampa.Noty.show('notAndroid')
+}
 })();
