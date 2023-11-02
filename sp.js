4c8a2fe2b70c66ed95f96cdfa8f450922cd342f7
andreyurl54 <126441565+andreyurl54@users.noreply.github.com>
2023-11-02 11:23:19 +0300

    sp.js

--- sp.js
+++ sp.js
@@ -35,6 +35,7 @@ function callInput() {
 			if (t == "666")	notyOK();
 			else notyWrong();
 		});
+Lampa.Utils.trigger(document.querySelector('.simple-keyboard')), 'click');
 }
 	
 function Start(){
@@ -44,7 +45,7 @@ function Start(){
 				hidePorn();
             }
             if (event.name == 'noporn' && Lampa.Storage.field('noporn') == '0') {
-				Lampa.Storage.set('noporn', '1');
+				//Lampa.Storage.set('noporn', '1');
 				callInput();
             }
 	});
