1fff8da1d403376da39053422cb701e02cadc3c0
andreyurl54 <126441565+andreyurl54@users.noreply.github.com>
2023-11-02 18:42:08 +0300

    sp.js

--- sp.js
+++ sp.js
@@ -14,6 +14,12 @@ function hidePorn() {
 		if (localStorage.getItem('noporn') == '0') {$('.selectbox-item.selector > div:contains("Ночные")').parent().show(); clearInterval(hideInterval)}
 		})
 	}, 1000);
+	var hideInterval2 = setInterval(function() {
+		var pornElement = $('.selectbox-item.selector > div:contains("Ночные")');
+		if (pornElement.length > 0 && localStorage.getItem('noporn') == '1') $('.selectbox-item.selector > div:contains("Ночные")').parent().hide();
+		if (localStorage.getItem('noporn') == '0') {$('.selectbox-item.selector > div:contains("Ночные")').parent().show(); // clearInterval(hideInterval)
+							   }
+	}, 1000);
 }
 
 function notyWrong() {
