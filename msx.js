0c8314a91537afa8f837c6bce2d2e8c594c792ef
andreyurl54 <126441565+andreyurl54@users.noreply.github.com>
2023-11-03 23:47:51 +0300

    msx.js

--- msx.js
+++ msx.js
@@ -1672,7 +1672,8 @@ if (Lampa.Storage.field('DIESEL_PlaylistVariant') == 'BITTV') {
      Lampa.Storage.listener.follow('change', function (event) {
         if (Lampa.Storage.field('Diesel_Auto_Start') == true) {       
             if (event.name == 'start_page' || 'activity') {
-                localStorage.setItem('activity', '{"id":0,"url":"' + diesel_playlist + '","title":"Дизель ТВ","groups":[],"currentGroup":"Russia","component":"diesel_iptv","page":1}');
+		var startGroup = Lampa.Activity.active().currentGroup;
+		localStorage.setItem('activity', '{"id":0,"url":"' + diesel_playlist + '","title":"Дизель ТВ","groups":[],"currentGroup":"' + startGroup +'","component":"diesel_iptv","page":1}');
                 localStorage.setItem('start_page', 'last');
             };
 			/*
