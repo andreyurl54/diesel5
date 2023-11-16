e07d2de6a9ac34ba2df77cf462303bc28f06d2fb
andreyurl54 <126441565+andreyurl54@users.noreply.github.com>
2023-11-16 11:31:15 +0300

    fullscreen.js

--- fullscreen.js
+++ fullscreen.js
@@ -1,8 +1,15 @@
 ;(function () {
 'use strict';
 setTimeout(function() {
+    var elem = document.documentElement;
+  // переход в полноэкранный режим
+  elem.requestFullscreen().catch(function() {
+    elem.webkitRequestFullscreen().catch(function() {
+      elem.msRequestFullscreen();
+    });
+  });
   //document.querySelector("div.head__action.selector.full-screen").trigger_click()
-  $("div.head__action.selector.full-screen").trigger('hover:enter');
+  //$("div.head__action.selector.full-screen").trigger('hover:enter');
   //Lampa.Utils.toggleFullscreen()
 },4000)
 })();
