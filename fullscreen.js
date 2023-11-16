78dd52755ce4eebb34a82b344d27beff22505d3e
andreyurl54 <126441565+andreyurl54@users.noreply.github.com>
2023-11-16 15:54:49 +0300

    fullscreen.js

--- fullscreen.js
+++ fullscreen.js
@@ -1,6 +1,10 @@
 (function () {
 'use strict';
 setTimeout(function() {
-  Lampa.Utils.toggleFullscreen()
+  Lampa.Utils.toggleFullscreen();
+  document.addEventListener("contextmenu", function(e) {
+        e.preventDefault(); window.history.back();
+        return false;
+  });
 },3000)
 })();
