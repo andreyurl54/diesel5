1ab908699b21ea9759ab4c379421b666511e9a87
andreyurl54 <126441565+andreyurl54@users.noreply.github.com>
2023-11-17 13:58:31 +0300

    menusort.js

--- menusort.js
+++ menusort.js
@@ -56,6 +56,7 @@ var icon_menu_sort = '<div class="settings-folder" style="padding:0!important"><
 						item.on('hover:enter', function () {
 							setTimeout(function() {
 								localStorage.setItem('menu_hide', []);
+								$(".menu__item").removeClass("hidden");
 							}, 0);
 						});
 					}
