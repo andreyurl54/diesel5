3193f8e3889211edbe8b45464bbf33226893a77a
andreyurl54 <126441565+andreyurl54@users.noreply.github.com>
2023-11-20 03:49:45 +0300

    autostart.js

--- autostart.js
+++ autostart.js
@@ -7,34 +7,34 @@ function pluginPrepare() {
 	
 function pluginStart() {
 	/* АвтоЗапуск */
-	pluginPrepare();
-	var proxyActivity = JSON.parse(localStorage.getItem('AutoStartActivity');
-	if (localStorage.getItem('AutoStartActivity') !== '') Lampa.Activity.push(proxyActivity)
-
+		pluginPrepare();
+		if (localStorage.getItem('AutoStartActivity')) {
+		    	if (localStorage.getItem('autoStart') == 'true') {
+			var proxyActivity = JSON.parse(localStorage.getItem('AutoStartActivity'));
+			Lampa.Activity.push(proxyActivity)
+			}
+		}
 	/* Меню */
-	Lampa.SettingsApi.addParam({
-						component: 'iptv',
-						param: {
-							name: 'autoStart', 	//название в Storage
-							type: 'trigger', 			//доступно select,input,trigger,title,static
-						},
-						field: {
-							name: 'Автозапуск с открытой категории', 	//Название подпункта меню
-							description: 'Lampa будет запущена с открытой сейчас страницы' //Комментарий к подпункту
-						},
-						onChange: function (value) {
-							var currentActivity = Lampa.Activity.active();
-							localStorage.setItem('AutoStartActivity', currentActivity);
-							Lampa.Settings.update();
-						}
-					});
+		Lampa.SettingsApi.addParam({
+							component: 'iptv',
+							param: {
+								name: 'autoStart',
+								type: 'trigger',
+							},
+							field: {
+								name: 'Автозапуск с открытой категории',
+								description: 'Lampa будет запущена с открытой сейчас страницы'
+							},
+							onChange: function (value) {
+								var currentActivity = JSON.stringify(Lampa.Activity.active());
+								localStorage.setItem('AutoStartActivity', currentActivity);
+								Lampa.Settings.update();
+							}
+						});
 }
 	
 if (!!window.appready) pluginStart();
 else Lampa.Listener.follow('app', function(e){if (e.type === 'ready') pluginStart()});
+
 })();
 
-/*
-var currentActivity = Lampa.Activity.active();
-localStorage.setItem('AutoStartActivity', currentActivity);
-*/
